b36452e38c38fd8b504d51b0901fb4b284e86e33
rim <rimdagdoug99@gmail.com>
2024-07-20 01:46:05 +0200

    feature/add-eslint

--- src/app/pages/eval/add-eval/add-eval.component.ts
+++ src/app/pages/eval/add-eval/add-eval.component.ts
@@ -9,18 +9,17 @@ import { User } from 'src/app/shared/interfaces/user.interface';
 @Component({
   selector: 'app-add-eval',
   templateUrl: './add-eval.component.html',
-  styleUrls: ['./add-eval.component.css']
+  styleUrls: ['./add-eval.component.css'],
 })
 export class AddEvalComponent {
-
   managers: User[] = [];
   developers: User[] = [];
-  errorMessage: string = '';
+  errorMessage = '';
 
   constructor(
     private evalService: EvalService,
     private userService: UserService,
-    private router: Router
+    private router: Router,
   ) {
     this.getManagers();
     this.getDevelopers();
@@ -33,30 +32,32 @@ export class AddEvalComponent {
   };
 
   getManagers() {
-    this.userService.getManagers().subscribe(managers => {
+    this.userService.getManagers().subscribe((managers) => {
       this.managers = managers;
     });
   }
 
   getDevelopers() {
-    this.userService.getDevelopers().subscribe(developers => {
+    this.userService.getDevelopers().subscribe((developers) => {
       this.developers = developers;
     });
   }
 
   addEval(evaluation: Evaluation): void {
     this.evalService.createeval(evaluation).subscribe(
-      evaluation => {
+      (evaluation) => {
         this.evaluation = evaluation;
         this.router.navigate(['/list-eval']);
       },
       (error: HttpErrorResponse) => {
         if (error.status === 500) {
-          this.errorMessage = 'Une erreur interne s\'est produite lors de l\'ajout de l\'évaluation.';
+          this.errorMessage =
+            "Une erreur interne s'est produite lors de l'ajout de l'évaluation.";
         } else {
-          this.errorMessage = 'Une erreur est survenue lors de l\'ajout de l\'évaluation.';
+          this.errorMessage =
+            "Une erreur est survenue lors de l'ajout de l'évaluation.";
         }
-      }
+      },
     );
   }
 }
