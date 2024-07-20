4d5f6f451cb4272abf0698b725efdf234ae9b1f0
rim <rimdagdoug99@gmail.com>
2024-07-21 00:45:13 +0200

    feature: add-guard

--- src/app/pages/eval/eval-routing.module.ts
+++ src/app/pages/eval/eval-routing.module.ts
@@ -4,20 +4,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddEvalComponent } from './add-eval/add-eval.component';
 import { EditEvalComponent } from './edit-eval/edit-eval.component';
 import { ListEvalComponent } from './list-eval/list-eval.component';
+import { authGuard } from 'src/app/services/auth.guard';
 
 
 const routes: Routes = [
     {
         path: 'add-eval',
-        component: AddEvalComponent
+        component: AddEvalComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'edit-eval/:id',
-        component: EditEvalComponent
+        component: EditEvalComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'list-eval',
-        component: ListEvalComponent
+        component: ListEvalComponent,
+        canActivate: [authGuard]
       }
       
 ];
